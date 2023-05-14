9fc9ea4c469e856688df76649c022692e21397ef
Pavel Foltyn <pavel.foltyn@volny.cz>
2023-05-14 02:48:18 +0200

    CHANGE Add 1st version of the DiffController implementation. Error handling, validation of input data, and Base64 encoding to be added.

--- RfeDiffSol/Rfe.DiffSvc.WebApi/Controllers/DiffController.cs
+++ RfeDiffSol/Rfe.DiffSvc.WebApi/Controllers/DiffController.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 using Rfe.DiffSvc.WebApi.Interfaces.Services;
-
+using Rfe.DiffSvc.WebApi.Helpers;
+using Rfe.DiffSvc.WebApi.BusinessObjects;
 
 
 namespace Rfe.DiffSvc.WebApi.Controllers
@@ -42,6 +43,71 @@ namespace Rfe.DiffSvc.WebApi.Controllers
         {
             _logger = logger;
             _diffService = diffService;
+        }
+
+
+
+        // Implement the core functionality first.
+        // TODO: Handle errors and validate input.
+        // TODO: Base64 encoding (to/from).
+
+
+
+        // GET /v1/diff/get-id
+        // Gets an ID for a new diff.
+        [HttpGet("get-id")]
+        public IActionResult GetId()
+        {
+
+            // Let the diff service generate a new id.
+            Guid id = _diffService.GenerateId();
+
+            // Wrap the ID in an (anonymous) object.
+            var dataWithID = new { id = id.ToString() };
+
+            // Return HTTP status code: 200 (OK)
+            // Insert data into the response body.
+            return Ok(dataWithID);
+
+        }
+
+
+
+        // POST /v1/diff/155a95ae-4132-470c-91b9-892a80cff59f/left
+        // Posts a left or right input data.
+        [HttpPost("{id:guid}/{position:regex(^((left)|(right))$)}")]
+        public IActionResult Post([FromRoute] Guid id, [FromBody] StreamInput streamInput, [FromRoute] string position)
+        {
+
+            // Parse the given diff operand position.
+            DiffOperandPosition positionFromEnum = ConvertHelper.ToDiffOperandPosition(position);
+
+            // Try to store data in the repo.
+            _diffService.SaveInput(id, streamInput, positionFromEnum);
+
+            // Wrap ID and position in an anonymous object.
+            var dataWithParams = new { id = id.ToString(), position = positionFromEnum.ToString() };
+
+            // Return HTTP status code: 201 (Created)
+            return Created(this.Request.Path, dataWithParams);
+
+        }
+
+
+
+        // GET /v1/diff/155a95ae-4132-470c-91b9-892a80cff59f
+        // Gets the result of the diff applied to "left" vs "right".
+        [HttpGet("{id:guid}")]
+        public IActionResult Get([FromRoute] Guid id)
+        {
+
+            // Get an output (result) for the requested id.
+            DiffOutput output = _diffService.GetOutput(id);
+
+            // Return HTTP status code: 200 (OK)
+            // Insert output into the response body.
+            return Ok(output);
+
         }
 
 
---
+++ RfeDiffSol/Rfe.DiffSvc.WebApi/Helpers/ConvertHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Rfe.DiffSvc.WebApi.BusinessObjects;
+
+
+
+namespace Rfe.DiffSvc.WebApi.Helpers
+{
+
+
+
+    /// <summary>
+    /// Exposes static methods for data conversions.
+    /// </summary>
+    public static class ConvertHelper
+    {
+
+
+
+        /// <summary>
+        /// Converts a given string into a <see cref="DiffOperandPosition"/> enum value.
+        /// </summary>
+        /// <param name="positionAsString">A DiffOperandPosition as a string.</param>
+        /// <returns>Returns an instance of DiffOperandPosition corresponding to the given string.</returns>
+        public static DiffOperandPosition ToDiffOperandPosition(string positionAsString)
+        {
+            // TODO: Integrity check - make sure the given constant exists in the enum.
+
+            // Parse the string. Try to get a corresponding value from values in the DiffOperandPosition enum.
+            // Disregard the upper/lower" case.
+            DiffOperandPosition position = (DiffOperandPosition)Enum.Parse(typeof(DiffOperandPosition), positionAsString, true);
+            return position;
+        }
+
+
+
+    }
+
+
+
+}
