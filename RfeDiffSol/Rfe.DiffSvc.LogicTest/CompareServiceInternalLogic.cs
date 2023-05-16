38ff170c9cdfe17a790d2dda12753d22bd9495c7
Pavel Foltyn <pavel.foltyn@volny.cz>
2023-05-16 12:12:09 +0200

    CHANGE Add a test project with unit test stubs for DiffRepo and CompareService.

---
+++ RfeDiffSol/Rfe.DiffSvc.LogicTest/CompareServiceInternalLogic.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+
+
+namespace Rfe.DiffSvc.LogicTest
+{
+
+
+
+    /// <summary>
+    /// Unit-tests methods of the CompareService class.
+    /// </summary>
+    [TestFixture]
+    public class CompareServiceInternalLogic
+    {
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+
+        [TearDown]
+        public void Teardown()
+        {
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+
+    }
+
+
+
+}
---
+++ RfeDiffSol/Rfe.DiffSvc.LogicTest/DiffRepoInternalLogic.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+
+
+
+namespace Rfe.DiffSvc.LogicTest
+{
+
+
+
+    /// <summary>
+    /// Unit-tests methods of the DiffRepo class.
+    /// </summary>
+    [TestFixture]
+    public class DiffRepoInternalLogic
+    {
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+
+        [TearDown]
+        public void Teardown()
+        {
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+
+    }
+
+
+
+}
